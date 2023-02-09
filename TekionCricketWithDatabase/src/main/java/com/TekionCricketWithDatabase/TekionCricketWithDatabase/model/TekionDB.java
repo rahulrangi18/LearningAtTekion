f1d2a1ab8df4c8dae5845e1ccc7f244b679df6f2
rahulrangi18 <rahulrangi18@gmail.com>
2023-02-09 14:22:26 +0530

    DB

---
+++ TekionCricketWithDatabase/src/main/java/com/TekionCricketWithDatabase/TekionCricketWithDatabase/model/TekionDB.java
@@ -0,0 +1,8 @@
+package com.TekionCricketWithDatabase.TekionCricketWithDatabase.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("TekionCricketDB")
+public class TekionDB {
+
+}
---
+++ TekionCricketWithDatabase/src/test/java/com/TekionCricketWithDatabase/TekionCricketWithDatabase/TekionCricketWithDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TekionCricketWithDatabase.TekionCricketWithDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekionCricketWithDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
