17beafa25b69d8d81a1548de12f00666cfc1c82d
Rahul Rangi <63532991+rahulrangi18@users.noreply.github.com>
2023-03-14 02:39:11 +0530

    Delete learnSpringAndDatabase directory

--- learnSpringAndDatabase/src/main/java/com/learnSpring/learnSpring/topic/TopicRepository.java
+++
@@ -1,7 +0,0 @@
-package com.learnSpring.learnSpring.topic;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
-public interface TopicRepository extends CrudRepository <Topic,String> {
-}
