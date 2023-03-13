f700338c066bab885f2da2e999ae72beecacb267
Rahul Rangi <63532991+rahulrangi18@users.noreply.github.com>
2023-03-14 02:39:24 +0530

    Delete springboot-mongodb directory

--- springboot-mongodb/src/main/java/com/rahulrangi/springbootmongodb/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rahulrangi.springbootmongodb.repositories;
-
-import com.rahulrangi.springbootmongodb.model.Book;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BookRepository extends MongoRepository<Book,Integer> {
-}
